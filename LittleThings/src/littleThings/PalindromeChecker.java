0b6b91cd80ba3bd204c4b10830b363d7e58ce7dd
Inigo <iartolozaga2@gmail.com>
2018-05-06 18:40:06 +0200

    palindromeChecker basic implementation - does not work when word is capitalized

--- LittleThings/src/littleThings/PalindromeChecker.java
+++ LittleThings/src/littleThings/PalindromeChecker.java
@@ -1,5 +1,40 @@
 package littleThings;
 
+import java.util.Scanner;
+
 public class PalindromeChecker {
-	//henlo
+	
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+		boolean done = false;
+		while (!done) {
+			System.out.println("Please write the word you want to check (dont capitalize):");
+			String word = in.nextLine();
+			if (isPalindrome(word)) {
+				System.out.println("Thats a palindrome!");
+			} else {
+				System.out.println("That's not a palindrome :(");
+			}
+		}
+		in.close();		
+	}
+	
+	public static boolean isPalindrome(String string) {
+		char[] charArray = string.toCharArray();
+		boolean result = true;
+		if (charArray.length % 2 == 0) {
+			for (int i = 0; i < charArray.length; i++) {
+				if (charArray[i] != charArray[charArray.length - i - 1]) {
+					result = false;
+				}
+			}
+		} else {
+			for (int j = 0; j < charArray.length - 1; j++) {
+				if (charArray[j] != charArray[charArray.length - j - 1]) {
+					result = false;
+				}
+			}
+		}
+		return result;
+	}
 }
