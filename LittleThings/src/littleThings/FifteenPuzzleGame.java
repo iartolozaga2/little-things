1def07dca12419bddbea13b62dc7ec672b5cbc1f
Inigo <iartolozaga2@gmail.com>
2018-05-07 13:32:19 +0200

    (rosettacode.org) FifteenPuzzleGame: COMPLETED

--- LittleThings/src/littleThings/FifteenPuzzleGame.java
+++ LittleThings/src/littleThings/FifteenPuzzleGame.java
@@ -1,5 +1,6 @@
 package littleThings;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,19 +9,26 @@ import java.util.stream.Collectors;
 
 public class FifteenPuzzleGame {
 
-	//Plays the game
-	public static void main (String[] args) {
+	// Plays the game
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		boolean done = false;
 		int[] board = setup();
-		System.out.println("INITIAL BOARD:\n" + Arrays.toString(board));
-		System.out.println("The 0 represents the empty space");
+		System.out.println("INITIAL BOARD:\n");
+		visualize(board);
+		System.out.println("\n\nThe 0 represents the empty space");
 		while (!done) {
-			System.out.println("What number do you want to move?");
+			visualize(board);
+			System.out.println("\n\nWhat number do you want to move?");
 			int move = in.nextInt();
-			if (validMove(board,Arrays.asList(board).indexOf(move),Arrays.asList(board).indexOf(0))) {
-				board[Arrays.asList(board).indexOf(0)] = move;
-				board[Arrays.asList(board).indexOf(move)] = 0;
+			if (move > 15 || move < 1) {
+				System.out.println("That number does not exist!");
+				continue;
+			}
+			if (validMove(board, asList(board).indexOf(move), asList(board).indexOf(0))) {
+				int iZero = asList(board).indexOf(0);
+				board[asList(board).indexOf(move)] = 0;
+				board[iZero] = move;
 				if (checkDone(board)) {
 					done = true;
 				}
@@ -33,8 +41,16 @@ public class FifteenPuzzleGame {
 			in.close();
 		}
 	}
-	
-	//Sets the board up in a random way (using Collections.shuffle())
+
+	private static List<Integer> asList(int[] board) {
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < board.length; i++) {
+			list.add(board[i]);
+		}
+		return list;
+	}
+
+	// Sets the board up in a random way (using Collections.shuffle())
 	public static int[] setup() {
 		int[] result = new int[16];
 		int counter = 0;
@@ -42,8 +58,8 @@ public class FifteenPuzzleGame {
 			result[i] = counter;
 			counter++;
 		}
-		//Converts to ArrayList<Integer>, shuffles, and then turns back into int[]
-		//Can probably be done in an easier way
+		// Converts to ArrayList<Integer>, shuffles, and then turns back into int[]
+		// Can probably be done in an easier way
 		List<Integer> list = Arrays.stream(result).boxed().collect(Collectors.toList());
 		Collections.shuffle(list);
 		int[] arr = list.stream().mapToInt(i -> i).toArray();
@@ -53,35 +69,26 @@ public class FifteenPuzzleGame {
 			return arr;
 		}
 	}
-	
-	//Checks whether a move is valid or not
-	//I felt physical pain while writing this
-	//99% sure there's a more efficient way to do this
+
+	// Checks whether a move is valid or not (i.e. if the desired number is next to
+	// zero)
 	public static boolean validMove(int[] board, int potential, int zero) {
-		switch (Arrays.asList(board).indexOf(potential)) {
-			case 0: return (Arrays.asList(board).indexOf(0) == 1 || Arrays.asList(board).indexOf(0) == 4);
-			case 1: return (Arrays.asList(board).indexOf(0) == 0 || Arrays.asList(board).indexOf(0) == 5 || Arrays.asList(board).indexOf(0) == 2);
-			case 2: return (Arrays.asList(board).indexOf(0) == 1 || Arrays.asList(board).indexOf(0) == 6 || Arrays.asList(board).indexOf(0) == 3);
-			case 3: return (Arrays.asList(board).indexOf(0) == 2 || Arrays.asList(board).indexOf(0) == 7);
-			case 4: return (Arrays.asList(board).indexOf(0) == 0 || Arrays.asList(board).indexOf(0) == 5 || Arrays.asList(board).indexOf(0) == 8);
-			case 5: return (Arrays.asList(board).indexOf(0) == 4 || Arrays.asList(board).indexOf(0) == 6 || Arrays.asList(board).indexOf(0) == 1 || Arrays.asList(board).indexOf(0) == 9);
-			case 6: return (Arrays.asList(board).indexOf(0) == 5 || Arrays.asList(board).indexOf(0) == 7 || Arrays.asList(board).indexOf(0) == 2 || Arrays.asList(board).indexOf(0) == 10);
-			case 7: return (Arrays.asList(board).indexOf(0) == 3 || Arrays.asList(board).indexOf(0) == 6 || Arrays.asList(board).indexOf(0) == 11);
-			case 8: return (Arrays.asList(board).indexOf(0) == 4 || Arrays.asList(board).indexOf(0) == 9 || Arrays.asList(board).indexOf(0) == 12);
-			case 9: return (Arrays.asList(board).indexOf(0) == 5 || Arrays.asList(board).indexOf(0) == 13 || Arrays.asList(board).indexOf(0) == 8 || Arrays.asList(board).indexOf(0) == 10);
-			case 10: return (Arrays.asList(board).indexOf(0) == 6 || Arrays.asList(board).indexOf(0) == 14 || Arrays.asList(board).indexOf(0) == 9 || Arrays.asList(board).indexOf(0) == 11);
-			case 11: return (Arrays.asList(board).indexOf(0) == 7 || Arrays.asList(board).indexOf(0) == 10 || Arrays.asList(board).indexOf(0) == 15);
-			case 12: return (Arrays.asList(board).indexOf(0) == 8 || Arrays.asList(board).indexOf(0) == 13);
-			case 13: return (Arrays.asList(board).indexOf(0) == 9 || Arrays.asList(board).indexOf(0) == 12 || Arrays.asList(board).indexOf(0) == 14);
-			case 14: return (Arrays.asList(board).indexOf(0) == 10 || Arrays.asList(board).indexOf(0) == 13 || Arrays.asList(board).indexOf(0) == 15);
-			case 15: return (Arrays.asList(board).indexOf(0) == 11 || Arrays.asList(board).indexOf(0) == 14);
-			default: return false;
-		}
+
+		int potX = potential % 4;
+		int potY = (potential - potX) / 4;
+
+		int zeroX = zero % 4;
+		int zeroY = (zero - zeroX) / 4;
+
+		int dx = (potX - zeroX) * (potX - zeroX);
+		int dy = (potY - zeroY) * (potY - zeroY);
+
+		return (dx == 1 || dy == 1) && !(dx == 1 && dy == 1);
 	}
-	
-	//Returns true if the player has completed the puzzle
+
+	// Returns true if the player has completed the puzzle
 	public static boolean checkDone(int[] board) {
-		//Creates a solved board
+		// Creates a solved board
 		int[] solvedBoard = new int[16];
 		int counter = 1;
 		for (int i = 0; i < 15; i++) {
@@ -89,12 +96,30 @@ public class FifteenPuzzleGame {
 			counter++;
 		}
 		solvedBoard[15] = 0;
-		//Compares the solved board with the potential one tile by tile
+		// Compares the solved board with the potential one tile by tile
 		for (int j = 0; j < 16; j++) {
 			if (solvedBoard[j] != board[j]) {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	public static void visualize(int[] board) {
+		for (int i = 0; i < board.length; i++) {
+			if (i % 4 == 0) {
+				if (board[i] / 10 == 0) {
+					System.out.print("\n" + "| " + board[i] + "  | ");
+				} else {
+					System.out.print("\n" + "| " + board[i] + " | ");
+				}
+			} else {
+				if (board[i] / 10 == 0) {
+					System.out.print(board[i] + "  | ");
+				} else {
+					System.out.print(board[i] + " | ");
+				}
+			}
+		}
 	}
 }
